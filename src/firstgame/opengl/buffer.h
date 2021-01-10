3f4e291694d4d34ed06876d16ec1b730a4e7414b
Natanael Rabello <natanael.rabello@outlook.com>
2021-01-10 11:16:07 -0300

    opengl Buffer and VertexArray only headers
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- src/firstgame/opengl/buffer.h
+++ src/firstgame/opengl/buffer.h
@@ -2,16 +2,21 @@
 #define FIRSTGAME_OPENGL_BUFFER_H_
 
 #include <utility>
+#include "gl/functions.h"
 
 namespace firstgame::opengl {
 
 /// Representation of a opengl buffer generated with glGenBuffers
 struct Buffer {
     /// Create and generate the buffer object
-    Buffer();
+    Buffer() { glGenBuffers(1, &id); }
 
     /// Delete buffer if non-zero
-    ~Buffer();
+    ~Buffer()
+    {
+        if (id)
+            glDeleteBuffers(1, &id);
+    }
 
     /// For creating a null Buffer
     struct Null {
@@ -20,7 +25,7 @@ struct Buffer {
     Buffer(Null) noexcept : id(0) {}
 
     /// Implicit cast to the buffer ID
-    operator unsigned int() const { return id; }
+    operator GLuint() const { return id; }
 
     /// Move constructor
     Buffer(Buffer&& other) noexcept : id(std::exchange(other.id, 0)) {}
@@ -37,7 +42,7 @@ struct Buffer {
     Buffer& operator=(const Buffer&) = delete;
 
    private:
-    unsigned int id;
+    GLuint id;
 };
 
 }  // namespace firstgame::opengl
--- src/firstgame/opengl/vertex_array.h
+++ src/firstgame/opengl/vertex_array.h
@@ -2,16 +2,22 @@
 #define FIRSTGAME_OPENGL_VERTEX_ARRAYS_H_
 
 #include <utility>
+#include "gl/types.h"
+#include "gl/functions.h"
 
 namespace firstgame::opengl {
 
 /// Representation of a opengl vertex array generated with glGenVertexArrays
 struct VertexArray {
     /// Create and generate a vertex array object
-    VertexArray();
+    VertexArray() { glGenVertexArrays(1, &id); }
 
     /// Delete buffer if non-zero
-    ~VertexArray();
+    ~VertexArray()
+    {
+        if (id)
+            glDeleteVertexArrays(1, &id);
+    }
 
     /// For creating a null VertexArray
     struct Null {
@@ -20,7 +26,7 @@ struct VertexArray {
     VertexArray(Null) noexcept : id(0) {}
 
     /// Implicit cast to the buffer ID
-    operator unsigned int() const { return id; }
+    operator GLuint() const { return id; }
 
     /// Move constructor
     VertexArray(VertexArray&& other) noexcept : id(std::exchange(other.id, 0)) {}
@@ -37,7 +43,7 @@ struct VertexArray {
     VertexArray& operator=(const VertexArray&) = delete;
 
    private:
-    unsigned int id;
+    GLuint id;
 };
 
 }  // namespace firstgame::opengl
