02968784d0c021292e949f6db3b718b05144f613
Natanael Rabello <natanael.rabello@outlook.com>
2021-01-16 22:24:12 -0300

    Fix Camera Perspective motion and vars
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- src/firstgame/render/camera_perspective.h
+++ src/firstgame/render/camera_perspective.h
@@ -14,22 +14,7 @@
 #include "firstgame/util/size.h"
 #include "view_projection.h"
 
-// temporary
-#include "firstgame/system/log.h"
-
 namespace firstgame::render {
-
-struct Perspective final {
-    float aspect_ratio_;
-    float fov_degrees_{ 45.0f };
-    float yaw_degrees_{ -90.0f };
-    float pitch_degrees_{ 0.0f };
-    glm::vec3 position_{ 0.0f, 0.0f, 5.0f };
-    glm::vec3 front_{ 0.0f, 0.0f, -1.0f };
-    glm::vec3 right_{ 0.0f, 0.0f, 0.0f };
-    glm::vec3 up_{ 0.0f, 1.0f, 0.0f };
-    ViewProjection matrix_{};
-};
 
 /// Perspective Camera provides projection of a 3D scene that mimics the way the human eye sees.
 /// The interface is designed to respond to input events filtered by a camera system.
@@ -67,12 +52,7 @@ class CameraPerspective final {
         pitch_degrees_ = std::min(pitch_degrees_, +89.0f);
         pitch_degrees_ = std::max(pitch_degrees_, -89.0f);
         // consolidate directional vectors
-        // forward_.x = cos(glm::radians(yaw_degrees_)) * cos(glm::radians(pitch_degrees_));
-        // forward_.y = sin(glm::radians(pitch_degrees_));
-        // forward_.z = sin(glm::radians(yaw_degrees_)) * cos(glm::radians(pitch_degrees_));
-        // forward_ = glm::normalize(forward_);
-        // right_ = glm::normalize(glm::cross(forward_, kWorldUp));
-        // up_ = glm::normalize(glm::cross(right_, forward_));
+        forward_ = CalculateForward();
         matrix_.view = CalculateView();
     }
 
@@ -82,10 +62,10 @@ class CameraPerspective final {
         const float velocity = kSpeed * deltatime;
         switch (direction) {
             case util::MoveDirection::Forward:
-                position_ += (glm::vec3(Forward().x, 0.0f, Forward().z) * velocity);
+                position_ += (glm::normalize(glm::vec3(forward_.x, 0.0f, forward_.z)) * velocity);
                 break;
             case util::MoveDirection::Backward:
-                position_ -= (glm::vec3(Forward().x, 0.0f, Forward().z) * velocity);
+                position_ -= (glm::normalize(glm::vec3(forward_.x, 0.0f, forward_.z)) * velocity);
                 break;
             case util::MoveDirection::Left: position_ -= (Right() * velocity); break;
             case util::MoveDirection::Right: position_ += (Right() * velocity); break;
@@ -100,18 +80,13 @@ class CameraPerspective final {
    private:
     [[nodiscard]] glm::mat4 CalculateView() const
     {
-        TRACE("position{{x: {}, y: {}, z: {}}}", position_.x, position_.y, position_.z);
-        // TRACE("forward_{{x: {}, y: {}, z: {}}}", forward_.x, forward_.y, forward_.z);
-        // TRACE("right_{{x: {}, y: {}, z: {}}}", right_.x, right_.y, right_.z);
-        // TRACE("up_{{x: {}, y: {}, z: {}}}", up_.x, up_.y, up_.z);
-        return glm::lookAt(position_, position_ + Forward(), Up());  //
+        return glm::lookAt(position_, position_ + forward_, Up());  //
     }
     [[nodiscard]] glm::mat4 CalculateProjection() const
     {
-        TRACE("fov: {}, aspect: {}", fov_degrees_, aspect_ratio_);
-        return glm::perspective(glm::radians(fov_degrees_), aspect_ratio_, +1.0f, -1.0f);
+        return glm::perspective(glm::radians(fov_degrees_), aspect_ratio_, 0.1f, 100.0f);
     }
-    [[nodiscard]] glm::vec3 Forward() const
+    [[nodiscard]] glm::vec3 CalculateForward() const
     {
         glm::vec3 forward{ 0.0f, 0.0f, -1.0f };
         forward.x = cos(glm::radians(yaw_degrees_)) * cos(glm::radians(pitch_degrees_));
@@ -119,11 +94,8 @@ class CameraPerspective final {
         forward.z = sin(glm::radians(yaw_degrees_)) * cos(glm::radians(pitch_degrees_));
         return glm::normalize(forward);
     }
-    [[nodiscard]] glm::vec3 Right() const
-    {
-        return glm::normalize(glm::cross(Forward(), kWorldUp));
-    }
-    [[nodiscard]] glm::vec3 Up() const { return glm::normalize(glm::cross(Right(), Forward())); }
+    [[nodiscard]] glm::vec3 Right() const { return glm::normalize(glm::cross(forward_, kWorldUp)); }
+    [[nodiscard]] glm::vec3 Up() const { return glm::normalize(glm::cross(Right(), forward_)); }
 
    private:
     static constexpr glm::vec3 kWorldUp{ 0.0f, 1.0f, 0.0f };
@@ -134,9 +106,7 @@ class CameraPerspective final {
     float yaw_degrees_{ -90.0f };
     float pitch_degrees_{ 0.0f };
     glm::vec3 position_{ 0.0f, 0.0f, 5.0f };
-    // glm::vec3 forward_{ 0.0f, 0.0f, -1.0f };
-    // glm::vec3 right_{ 0.0f, 0.0f, 0.0f };
-    // glm::vec3 up_{ 0.0f, 1.0f, 0.0f };
+    glm::vec3 forward_{ 0.0f, 0.0f, -1.0f };
     ViewProjection matrix_{};
 };
 
